d7010285cfe7ceafdc8e3160595a6051609e9ef8
augustoprogramador <augustoprogramador@hotmail.com>
2023-04-12 16:06:57 -0300

    terceira aula

---
+++ 03/aula.js
@@ -0,0 +1,24 @@
+const notas = [
+    4, 5, 8
+];
+
+console.log(classificaAluno());
+
+function calculaMedia() {
+    let somaNotas = 0;
+    for (const nota of notas) {
+        somaNotas += nota;
+    }
+    return somaNotas / notas.length;
+}
+
+function classificaAluno() {
+    const media = calculaMedia().toFixed(2);
+    if (media < 5) {
+        return 'REPROVADO';
+    }
+    if (media > 5 && media <= 7) {
+        return 'RECUPERAÇÃO';
+    }
+    return 'APROVADO';
+}
