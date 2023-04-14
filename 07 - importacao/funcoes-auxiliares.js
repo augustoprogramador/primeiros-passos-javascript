ac2848c7a4b045a45ab1b0b3b7038d8ed313e8b1
augustoprogramador <augustoprogramador@hotmail.com>
2023-04-14 17:21:55 -0300

    aula 7

---
+++ 07 - importacao/funcoes-auxiliares.js
@@ -0,0 +1,12 @@
+
+const numSorteados = [5, 50, 10, 98, 23];
+
+function get() {
+    return 10;
+}
+
+function print(texto) {
+    console.log(texto);
+}
+
+module.exports = {get, print};
---
+++ 07 - importacao/main.js
@@ -0,0 +1,16 @@
+
+const { get, print } = require('./funcoes-auxiliares');
+
+/**
+ * Uma sala contém 5 alunos e para cada aluno foi sorteado um número de 0 - 100.
+ * Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior sorteado.
+ * 
+ * Entrada:
+ * 5, 50, 10, 98, 23
+ * 
+ * Saída: 
+ * 98
+ * 
+ */
+
+print(get())
